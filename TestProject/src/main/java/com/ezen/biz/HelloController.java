69eee8a9b21230b32aa062b8fb775147fb59d5fb
annie <atthe09624@gmail.com>
2023-11-16 14:23:38 +0900

    HelloController 완료

---
+++ TestProject/src/main/java/com/ezen/biz/HelloController.java
@@ -0,0 +1,10 @@
+package com.ezen.biz;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+	public void hello() {
+		System.out.println("hello");
+	}
+}
